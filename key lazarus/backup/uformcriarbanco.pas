34821d8b939521ce0d7622c3be8cbb9233b381bb
affonsoelias <affonso_elias@hotmail.com>
2022-04-27 18:30:08 -0300

    Add files via upload
    Upload do código

---
+++ key lazarus/backup/uformcriarbanco.pas
@@ -0,0 +1,65 @@
+unit uFormCriarBAnco;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  DBCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    procedure Edit1Change(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+    procedure Panel1Click(Sender: TObject);
+    procedure Panel2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm2 }
+
+
+
+procedure TForm2.Edit1Change(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Label1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Panel1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm2.Panel2Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
